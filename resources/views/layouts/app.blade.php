2ea0d8dd5e854ac303c4f54ef59e80e8b0fc7b1f
Diablo Volfir <lesteripulansible@gmail.com>
2024-07-25 23:05:40 +0800

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,7 +78,6 @@
     <script src="{{ asset('js/admin/payment-method-table.js') }}"></script>
     <script src="{{ asset('js/SidebarData.js') }}"></script>
     @include('layouts.script')
-    <script src="{{ asset('js/components/builds/header.js') }}" ></script>
 <script src="{{ asset('js/app.js') }}" defer></script>
 
 <!--<script src="{{ asset('vendor/datatables/buttons.server-side.js') }}"></script>-->
